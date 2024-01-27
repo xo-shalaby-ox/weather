e5014ac5b84aba4871f940aec75794854d975066
mohamed shalaby <mcoding98@gmail.com>
2024-01-28 01:57:02 +0200

    first app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+import { WeatherApp } from "./Components/Weather App/WeatherApp";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
